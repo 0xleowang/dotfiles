38c8945ab6cd9ea1bf05c89d32c74aa42ec76460
Leo Wang <5508123+0xleowang@users.noreply.github.com>
2023-01-01 14:43:58 +1100

    Surpress output when adding Homebrew ZSH to /etc/shells

--- install_zsh.zsh
+++ install_zsh.zsh
@@ -6,7 +6,7 @@ echo "\n<<< Installing ZSH >>>\n"
 # Changing the default shell to the ZSH installed by Homebrew
 
 echo "Enter superuser password to edit /etc/shells"
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells'
+echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 
 echo "Enter user password to change login shell"
 chsh -s '/usr/local/bin/zsh'
