7078adcef7c06ecbdc69b37092b32041ea3dbae2
Leo Wang <5508123+0xleowang@users.noreply.github.com>
2023-01-01 15:19:06 +1100

    Add conditions to install_zsh.zsh

--- install_zsh.zsh
+++ install_zsh.zsh
@@ -5,9 +5,13 @@ echo "\n<<< Installing ZSH >>>\n"
 # Installation was carried in the Brewfile
 # Changing the default shell to the ZSH installed by Homebrew
 
-echo "Enter superuser password to edit /etc/shells"
-echo '/usr/local/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+if ! grep -Fxq /usr/local/bin/zsh /etc/shells; then
+  echo "Enter superuser password to edit /etc/shells"
+  echo "/usr/local/bin/zsh" | sudo tee -a /etc/shells >/dev/null
+fi
 
-echo "Enter user password to change login shell"
-chsh -s '/usr/local/bin/zsh'
+if [ $SHELL = "/bin/zsh" ]; then
+  echo "Enter user password to change login shell"
+  chsh -s '/usr/local/bin/zsh'
+fi 
 
