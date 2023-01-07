35aa85313503950d1a5924c4aa2a5d1e302c8150
Leo Wang <5508123+0xleowang@users.noreply.github.com>
2023-01-07 21:36:08 +1100

    Add tokyonight color scheme with transparent background

--- .config/nvim/lua/user/init.lua
+++ .config/nvim/lua/user/init.lua
@@ -7,5 +7,22 @@ local config = {
     ' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
     ' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
   },
+
+  colorscheme = "tokyonight",
+
+  plugins = {
+    init = {
+      -- ColorScheme
+      ["folke/tokyonight.nvim"] = {
+        as = "tokyonight",
+        config = function()
+          require("tokyonight").setup {
+            style = "storm",
+            transparent = true,
+          }
+        end,
+      },
+    },
+  },
 }
 return config
